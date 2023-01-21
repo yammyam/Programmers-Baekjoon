9a02775b2f782f3550ff3badcd839dea07494034
yammyam <96424434+yammyam@users.noreply.github.com>
2023-01-22 00:37:30 +0900

    [level 0] Title: 모음 제거, Time: 0.12 ms, Memory: 33.5 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120849. 모음 제거/모음 제거.js
@@ -0,0 +1,9 @@
+function solution(my_string) {
+    var answer = '';
+    for(let i = 0 ; i<my_string.length ; i++){
+        if(my_string[i]!=='a'&& my_string[i]!=='e'&& my_string[i]!=='i' && my_string[i]!=='o'&& my_string[i]!=='u'){
+         answer = answer + my_string[i];   
+        }
+    }
+    return answer;
+}
