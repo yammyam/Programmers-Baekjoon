a8ff6d6dbc2badd0d6c74cc137e26eb645167369
yammyam <96424434+yammyam@users.noreply.github.com>
2023-05-21 02:19:00 +0900

    [Bronze V] Title: A/B, Time: 120 ms, Memory: 9596 KB -BaekjoonHub

--- 백준/Bronze/1008. A／B/A／B.js
+++ 백준/Bronze/1008. A／B/A／B.js
@@ -1,7 +1,3 @@
-let fs = require("fs");
-let input = fs.readFileSync("/dev/stdin").toString().split(" ");
+let input = require("fs").readFileSync("/dev/stdin").toString().split(" ");
 
-let a = parseInt(input[0]);
-let b = parseInt(input[1]);
-
-console.log(a / b);
+console.log(parseFloat(input[0]) / parseFloat(input[1]));
